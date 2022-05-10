7980e47adc2c202fa47b7ac870b41bd664b69564
Nadeem Sarwar <na.sarwar@gemsol.in>
2022-05-10 15:10:01 +0530

    Second changes

--- ExpressJs/staticWebsite/index.js
+++ ExpressJs/staticWebsite/index.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const hbs = require("hbs");
 const port = 8080;
 
 //Path
  console.log(path.join(__dirname,'../public'));
-const absPath = path.join(__dirname,'../public');  
+const absPath = path.join(__dirname,'../public'); 
+const templatePath = path.join(__dirname,'../templates/views');
+const partialsPath = path.join(__dirname,'../templates/partials');  
 
 
 // to set the view engine
 app.set("view engine",'hbs');
+
+//to rename views directory
+app.set("views",templatePath);
+
+// Partials
+hbs.registerPartials(partialsPath);
 
 // Built-in middleware
 //app.use(express.static(absPath));
@@ -19,9 +28,23 @@ app.get("",(req,res) =>{
    res.render('index');
 });
 
-app.get("/", (req, res) => {
+app.get("/about",(req,res) =>{
+    res.render('aboutus');
+ });
+
+/* app.get("/", (req, res) => {
     res.send("Hello from express server");
 
+}) */
+app.get("/about/*",(req, res) =>{
+    res.render("404",{
+        errorcomment : "Opps this about us page couldn't found"
+    });
+})
+app.get('*',(req, res) =>{
+    res.render("404",{
+        errorcomment : "Opps page couldn't found"
+    });
 })
 
 app.listen(port, ()=>{
